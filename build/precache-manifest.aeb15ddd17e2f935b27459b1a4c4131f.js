e863fdb57774cf75e8ccf2d2469df9bcf3cb45e8
Julian <julian.tapales@gmail.com>
2020-06-16 10:02:45 +0100

    gitignore

---
+++ build/precache-manifest.aeb15ddd17e2f935b27459b1a4c4131f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dfb17abbadd56d5987c",
+    "url": "/static/css/main.5467eed3.chunk.css"
+  },
+  {
+    "revision": "1dfb17abbadd56d5987c",
+    "url": "/static/js/main.b6a83f13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e952c0074dc85f402c7",
+    "url": "/static/css/2.567a597d.chunk.css"
+  },
+  {
+    "revision": "0e952c0074dc85f402c7",
+    "url": "/static/js/2.f845cf70.chunk.js"
+  },
+  {
+    "revision": "5d2fab847f9e7fd8b3ba958ec03418d5",
+    "url": "/index.html"
+  }
+];
